4d1a7427635058117a963d65894233b532997e91
jacksonsanger <117399219+jacksonsanger@users.noreply.github.com>
2024-12-27 01:08:35 -0700

    update

--- src/hw/BadAvatarException.java
+++
@@ -1,7 +0,0 @@
-package hw;
-
-public class BadAvatarException extends Exception {
-	public BadAvatarException(String message) {
-		super(message);
-	}
-}
--- src/hw/Drawable.java
+++
@@ -1,7 +0,0 @@
-package hw;
-
-import edu.du.dudraw.Draw;
-
-public interface Drawable {
-	public void draw(Draw duDwin);
-}
